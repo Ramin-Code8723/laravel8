a8644555f176f106385b6ab0d3120c34a24e54b2
Ramin-Code8723 <151490266+Ramin-Code8723@users.noreply.github.com>
2023-11-20 12:14:13 +0700

    w2

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>Ramil Srikanha 65122420013</h1>
+        <p1>ant <a herf="gallery/ant>"><img src="{{$ant}}" style="width:150px;height:90px"></a></p1>
+<p1>ant <a herf="gallery/god>"><img src="{{$god}}" style="width:150px;height:90px"></a></p1>
+<p1>ant <a herf="gallery/bird>"><img src="{{$bird}}" style="width:150px;height:90px"></a></p1>
+<p1>ant <a herf="gallery/cat>"><img src="{{$cat}}" style="width:150px;height:90px"></a></p1>
+<p1>ant <a herf="gallery/spider>"><img src="{{$spider}}" style="width:150px;height:90px"></a></p1>
+    </body>
+</html>
